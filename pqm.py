c26d119b0cd263eb4aae0e7559a81b79339ef28b
Rodrigo <xrodssx@hotmail.com>
2018-07-18 13:57:59 -0300

    Add pqm file

---
+++ pqm.py
@@ -0,0 +1,56 @@
+import numpy as np
+from scipy.special import binom
+
+# Computes the Hamming distance
+def hamming_distance(u, v):
+    diff_n = 0
+    
+    for value1, value2 in zip(u, v):
+        if value1 != value2:
+            diff_n += 1
+    
+    return diff_n
+
+# Equation
+def memory_retrieval(input_pattern, patterns, control_bits_n, nvalue=1):
+    i = input_pattern
+    pi = np.pi
+    b = control_bits_n
+    p = len(patterns)
+    n = len(input_pattern)
+    
+    #Probabilities array
+    p_array = []
+    
+    #For each number of 1 bits
+    for l in range(b+1):
+        amp = binom(b, l) * (1/p)
+        sum_value = 0
+        for k in range(p):
+            dh = hamming_distance(i, patterns[k])
+            v = (pi/(2*n * nvalue))*dh
+            
+            #sum_value += ((np.cos(v)**(b-l))**2) * ((np.sin(v)**l)**2)
+            sum_value += (np.cos(v)**(2*b-2*l)) * (np.sin(v)**(2*l))
+            
+        p_array.append(amp * sum_value)
+    
+    return p_array
+
+# Equation 1
+def mem_retrieval_1cbit(input_pattern, patterns, nvalue=1):
+    i = input_pattern
+    pi = np.pi
+    p = len(patterns)
+    n = len(input_pattern)
+    
+    amp = 1/p
+    
+    sum_value = 0
+    for k in range(p):
+        dh = hamming_distance(i, patterns[k])
+        v = (pi/(2*n * nvalue))*dh
+        
+        sum_value += np.cos(v)**2
+    
+    return amp * sum_value
