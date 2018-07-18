cd0def3028f17e77d37112644c9d08d0f3930ffe
Rodrigo <xrodssx@hotmail.com>
2018-07-18 16:47:10 -0300

    Improve load function

--- util.py
+++ util.py
@@ -1,6 +1,13 @@
 import csv
 
-def load_dataset(name):
+def load_dataset(name, label_size):
     folder = 'dataset/'
+    
     with open(folder+name) as file:
-        return [[int(value) for value in row] for row in csv.reader(file)]
+        X = []
+        y = []
+        for row in csv.reader(file):
+            X.append([int(row[i]) for i in range(len(row)-label_size)])
+            y.append([int(row[i]) for i in range(len(row)-label_size, len(row))])
+        
+        return X, y
