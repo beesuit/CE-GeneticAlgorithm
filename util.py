42bdc43ab6790b5cab2a89f123510a65058a292f
Rodrigo <xrodssx@hotmail.com>
2018-07-19 11:51:17 -0300

    Add hamming distance function to util file

--- util.py
+++ util.py
@@ -11,3 +11,13 @@ def load_dataset(name, label_size):
             y.append([int(row[i]) for i in range(len(row)-label_size, len(row))])
         
         return X, y
+
+# Computes the Hamming distance
+def hamming_distance(u, v):
+    diff_n = 0
+    
+    for value1, value2 in zip(u, v):
+        if value1 != value2:
+            diff_n += 1
+    
+    return diff_n
