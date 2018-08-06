6328378f932048311fc05379aa22b2baa33a0088
Rodrigo <xrodssx@hotmail.com>
2018-08-06 13:06:13 -0300

    Add best_solution

--- Experiment.py
+++ Experiment.py
@@ -5,12 +5,21 @@ class Experiment(object):
     def __init__(self, name):
         self.name = name
         self.results = []
+        self.best_solution = None
         
     def run(self, ga, n):
         self.results.clear()
         for i in range(n):
             result = ga.run()
             self.results.append(result)
+            
+            best = ga.best_solution
+            if self.best_solution != None:
+                if ga.problem.best(best.fitness, self.best_solution['fitness']):
+                    self.best_solution = {'params':ga.problem.decode_solution(best.chromossome), 'fitness': best.fitness}
+            else:
+                self.best_solution = {'params':ga.problem.decode_solution(best.chromossome), 'fitness': best.fitness}
+            
         
         return self.results
     
